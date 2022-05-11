6c4a081b1eb24ca45fbec1346c81a529e1db3249
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-11 10:48:35 -0700

    Formik

---
+++ components/FormikScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Formik } from "formik";
+
+import { useTheme, useData } from "../hooks";
+import {
+  Block,
+  Button,
+  Input,
+  Image,
+  Switch,
+  Modal,
+  Text,
+} from "../components/";
+
+const FormikScreen = () => {
+  const { assets, colors, gradients, sizes } = useTheme();
+
+  return (
+    <Formik initialValues={{ Name: "" }} onSubmit={(text) => console.log(text)}>
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <Block marginTop={sizes.xxl * 4} marginHorizontal={sizes.m}>
+          <Input
+            placeholder="Enter data"
+            onChangeText={handleChange("Name")}
+            onBlur={handleBlur("Name")}
+            value={values.Name}
+          />
+          <Button
+            marginTop={sizes.m}
+            height={sizes.xxl}
+            onPress={handleSubmit}
+            primary
+          >
+            <Text h5 white>
+              Submit
+            </Text>
+          </Button>
+        </Block>
+      )}
+    </Formik>
+  );
+};
+
+export default FormikScreen;
