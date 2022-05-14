a6629a70d6ed4b033547221c93066112e0319c3f
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-05-14 15:36:18 -0700

    operators component

--- components/Operators.js
+++ components/Operators.js
@@ -1,6 +1,8 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
+//oprators components starts here
+
 const Operators = (props) => {
   return (
     <View style={styles.container}>
